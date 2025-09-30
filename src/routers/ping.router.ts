60a9ed5f931b2a3017af7dcfaedcfe6e185f9f5f
nandan kumar <nandanbharadwaj4@gmail.com>
2025-09-30 09:20:26 +0530

    Added routing and controller layer

--- src/routers/ping.router.ts
+++ src/routers/ping.router.ts
@@ -4,16 +4,7 @@ import { pingHandler } from '../controllers/ping.controller';
 
 const pingRouter = express.Router();
 
-function middleware1(req: Request, res: Response, next: NextFunction) {
-    console.log('Middleware 1 executed');
-    next(); // call the next middleware or route handler
-}
 
-function middleware2(req: Request, res: Response, next: NextFunction) {
-    console.log('Middleware 2 executed');
-    next(); // call the next middleware or route handler
-}
-
-pingRouter.get('/ping', middleware1, middleware2, pingHandler);   // middleware1 ---> middleware2 ---> pingHandler
+pingRouter.get('/ping', pingHandler);   // middleware1 ---> middleware2 ---> pingHandler
 
 export default pingRouter;
