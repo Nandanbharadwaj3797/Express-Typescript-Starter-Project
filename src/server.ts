1c73ea83ece537c91c53e3d6d9da15303a1988d8
nandan kumar <nandanbharadwaj4@gmail.com>
2025-10-01 11:21:52 +0530

    Added error handler

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
-const {serverConfig} = require('./config');
+import { serverConfig } from './config';
 import v1Router from './routers/v1/index.router';
 import v2Router from './routers/v2/index.router';
+import { genericErrorHandler } from './middlewares/error.middleware';
 
 
 const app = express();
 
+// Add body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * Registering all the routers and their corresponding routes with out app server object.
@@ -13,6 +17,7 @@ const app = express();
 
 app.use('/api/v1', v1Router);
 app.use('/api/v2', v2Router);
+app.use(genericErrorHandler);
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server is running on http://localhost:${serverConfig.PORT}`);
